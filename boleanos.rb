e729430e2eadd0505f0cadf4aedbf3c5dcfc14d4
Angelo <angelobj91@gmail.com>
2025-02-04 23:43:40 -0300

    new_boleanos

---
+++ boleanos.rb
@@ -0,0 +1,21 @@
+'''
+AND
+Verdade e Verdade = Verdade
+Verdade e Falso = Falso
+Falso e Falso = Falso
+Falso e Verdade = Falso
+
+OR
+Verdade ou Verdade = Verdade
+Verdade ou Falso = Falso
+Falso ou Falso = Falso
+Falso ou Verdade = Falso
+
+NOT
+Não Verdade = Falso
+Não Falso = Verdade
+'''
+
+puts false && true
+puts false || true
+puts !false && true
