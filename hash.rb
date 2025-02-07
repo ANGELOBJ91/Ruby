e5596e33923c88cbdd119a04eb21a59960920092
Angelo <angelobj91@gmail.com>
2025-02-07 16:57:55 -0300

    <feat>hash

---
+++ hash.rb
@@ -0,0 +1,10 @@
+hash = {nome: "Angelo" , idade: 34}
+# or  hash1 = Hash.new
+hash[:altura] = 1.78
+hash.delete (:idade)
+#puts hash.clear(deleta tudo que esta em chaves)
+#puts hash.keys (mostra as variaveis que possui )
+#puts hash.values (mostra os valores que estão nas chaves)
+#puts hash.size (mostra o tamanho)
+#puts hash.length
+puts hash.length
