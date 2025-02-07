9b6240414ec54cf47255cfc17c067aef17a8305c
Angelo <angelobj91@gmail.com>
2025-02-07 17:35:47 -0300

    <refactor>for

--- Repeticao/for.rb
+++ Repeticao/for.rb
@@ -2,6 +2,13 @@ nomes = ["joao", "maria", "josé", "matheus"]
 
 dict = {nome: "Angelo", idade: 34, altura: 1.78}
 
-for nome in nomes do
-  puts nome
+# for nome in nomes do
+#  puts nome
+# end
+
+# {k} quer dizer keys (variaves que estão em chaves)
+# {v} quer dizer values (valores que estão em chaves)
+
+for k, v in dict do
+  puts "#{k}: #{v}"
 end
