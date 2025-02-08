bf485554ad6e9b0bf96ba0719168181ba70ab884
Angelo <angelobj91@gmail.com>
2025-02-08 00:04:27 -0300

    <feat>calculo

---
+++ metodos/calculo.rb
@@ -0,0 +1,20 @@
+def soma(x, y, z)      #podemos utilizar com ou sem ()    
+  return x + y + z            # podemos não utilizar o return 
+end
+
+def subt( x, y)
+  x - y
+end
+
+def mult(x, y)
+  x * y
+end
+
+def div (x, y)
+  x / y
+end
+
+puts soma 2, 4, 5
+puts subt 5, 6
+puts mult 3, 9
+puts div 10, 5
